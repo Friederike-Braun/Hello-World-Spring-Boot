32ab553ddf49a76f98e93b4ec8c69fa31dda7dd7
Friederike Braun <fbraun@haeger-consulting.de>
2021-12-13 14:46:34 +0100

    removed unused import

--- src/main/java/de/haegerconsulting/training/friederike/HelloWorldSpringBoot/Controller.java
+++ src/main/java/de/haegerconsulting/training/friederike/HelloWorldSpringBoot/Controller.java
@@ -1,6 +1,5 @@
 package de.haegerconsulting.training.friederike.HelloWorldSpringBoot;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
